4e43a25c5fa6625fc35369a874863bff088cbba0
dashborr <danisarfas7@gmail.com>
2026-03-09 15:58:55 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if(x > 5){
+        cout << "Angka besar = " << x;
+    }else{
+        cout << "Angka kecil = " << x;
+    }
+}
